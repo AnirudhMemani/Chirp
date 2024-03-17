1d4c2031a9a98c1bd1dbfb7c0b26e6d27c0d28d9
Anirudh Memani <nemanianirudh@gmail.com>
2024-03-18 05:06:09 +0530

    fixes

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,52 +1,80 @@
-import { useEffect, useRef, useState } from "react"
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import { Room } from "./Room";
+import { Button, Input } from "@material-tailwind/react";
 
 export const Landing = () => {
-    const [name, setName] = useState("");
-    const [localAudioTrack, setLocalAudioTrack] = useState<MediaStreamTrack | null>(null);
-    const [localVideoTrack, setlocalVideoTrack] = useState<MediaStreamTrack | null>(null);
-    const videoRef = useRef<HTMLVideoElement>(null);
+  const [name, setName] = useState("");
+  const [localAudioTrack, setLocalAudioTrack] =
+    useState<MediaStreamTrack | null>(null);
+  const [localVideoTrack, setlocalVideoTrack] =
+    useState<MediaStreamTrack | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-    const [joined, setJoined] = useState(false);
+  const [joined, setJoined] = useState(false);
 
-    const getCam = async () => {
-        const stream = await window.navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        })
-        // MediaStream
-        const audioTrack = stream.getAudioTracks()[0]
-        const videoTrack = stream.getVideoTracks()[0]
-        setLocalAudioTrack(audioTrack);
-        setlocalVideoTrack(videoTrack);
-        if (!videoRef.current) {
-            return;
-        }
-        videoRef.current.srcObject = new MediaStream([videoTrack])
-        videoRef.current.play();
-        // MediaStream
+  const getCam = async () => {
+    const stream = await window.navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    // MediaStream
+    const audioTrack = stream.getAudioTracks()[0];
+    const videoTrack = stream.getVideoTracks()[0];
+    setLocalAudioTrack(audioTrack);
+    setlocalVideoTrack(videoTrack);
+    if (!videoRef.current) {
+      return;
     }
+    videoRef.current.srcObject = new MediaStream([videoTrack]);
+    videoRef.current.play();
+  };
 
-    useEffect(() => {
-        if (videoRef && videoRef.current) {
-            getCam()
-        }
-    }, [videoRef]);
+  useEffect(() => {
+    if (videoRef && videoRef.current) {
+      getCam();
+    }
+  }, [videoRef]);
 
-    if (!joined) {
-            
-    return <div>
-            <video autoPlay ref={videoRef}></video>
-            <input type="text" onChange={(e) => {
-                setName(e.target.value);
-            }}>
-            </input>
-            <button onClick={() => {
-                setJoined(true);
-            }}>Join</button>
+  if (!joined) {
+    return (
+      <div className="h-dvh flex justify-center items-center gap-6 w-full">
+        <video
+          autoPlay
+          ref={videoRef}
+        ></video>
+        <div className="flex gap-5">
+          <Input
+            crossOrigin={undefined}
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            label="Write your name"
+            className="pr-20"
+            containerProps={{
+              className: "min-w-0",
+            }}
+          />
+          <Button
+            placeholder={undefined}
+            className="rounded"
+            onClick={() => {
+              setJoined(true);
+            }}
+          >
+            Join
+          </Button>
         </div>
-    }
+      </div>
+    );
+  }
 
-    return <Room name={name} localAudioTrack={localAudioTrack} localVideoTrack={localVideoTrack} />
-}
+  return (
+    <Room
+      name={name}
+      localAudioTrack={localAudioTrack}
+      localVideoTrack={localVideoTrack}
+    />
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@material-tailwind/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider>
     <App />,
-)
+  </ThemeProvider>
+);
